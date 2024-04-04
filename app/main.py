9133bad2fffa6764819e6a642d11b2c39f8b5575
MaximilianoBury <mbury@legalario.com>
2024-04-04 20:20:25 -0300

    update prompts

--- app/main.py
+++ app/main.py
@@ -59,11 +59,11 @@ async def process_word(uploaded_file: UploadFile = File(...)):
             [
                 SystemMessage(
                         content="""Extrae la informacion del documento en caso de que exista, sino se encuentra aclara que la informacion no es proporcionada en el documento:
-                Representante legal:Localiza y captura el nombre completo del representante legal que otorga el poder notarial. Si no se encuentra el nombre, indica que no está disponible,
+                Representante legal: Localiza el nombre completo de alguno o varios representantes legales o apoderados que son los que se solicitan al notario el otorgamieto del muevo poder estos deben de estar al principio del documentos necesariamente en los primeros parrafos.  Si no se encuentra el nombre, indica que no está disponible,
                 Apoderado: Identifica y extrae el nombre completo de la persona a quien se le otorga el poder para actuar en nombre del representante legal. Si no se proporciona el nombre, señala que no se incluyó,
                 Poderes Otorgados:  Busca y registra los detalles específicos sobre los poderes otorgados al apoderado, incluyendo las acciones que está autorizado a realizar en nombre del representante legal. Si no se especifican los poderes, menciona que no se encontró esa información,
                 Fecha de otorgamiento: Localiza y anota la fecha en que se otorgó el poder notarial. Si no se indica una fecha de otorgamiento, aclara que no se proporcionó,
-                Fecha de vigencia: Captura la fecha de inicio y fin de la validez del poder notarial, si se especifican. Si no se mencionan las fechas de vigencia, indica que no se incluyeron,
+                Cual es la vigencia del poder notarial otrogado en este documento puede ser vigencia indefinida o por ciertos años, por ley tiene que encontrarse en el documento, generalmente la información relevante está en la sección que dice: "La presente Autorización tiene una vigencia ..",
                 Número de protocolo: Identifica y registra el número de protocolo del poder notarial, si se proporciona. Si no se encuentra el número de protocolo, señala que no se incluyó,
                 Datos notariales: Extrae la información sobre el notario que certificó el poder notarial, incluyendo su nombre completo, número de registro y ubicación de su notaría, si se proporcionan. Si alguno de estos datos falta, indica cuál no se encontró,
                 Firma y sello: Verifica si el poder notarial incluye la firma y sello del notario que lo certificó y menciona las respectivas firmas. Si no se encuentran la firma y sello, menciona que no se incluyeron en el documento proporcionado.
@@ -225,7 +225,7 @@ async def process_acta(uploaded_file: UploadFile = File(...)):
                     CLABE Bancaria:  Localiza y captura la CLABE (Clave Bancaria Estandarizada) interbancaria, que es un código único que identifica la cuenta bancaria en el sistema financiero mexicano. Si no se proporciona la CLABE, indica que no se incluyó,
                     Sucursal Bancaria: Si la cuenta está asociada a una sucursal bancaria específica, busca y registra la información sobre dicha sucursal, como su nombre, dirección o código. Si no se menciona la sucursal bancaria, señala que no se encontró,
                     Fecha de Emisión:  Identifica y anota la fecha en que se emitió la carátula bancaria. Si no se especifica la fecha de emisión, indica que no se proporcionó,
-                    Firma y Sello de la Institución Bancaria: Verifica si la carátula bancaria incluye la firma y el sello oficial del banco emisor para validar la autenticidad del documento. Si no se encuentran la firma y el sello, menciona que no se incluyeron en el documento proporcionado.
+                    Firma y Sello de la Institución Bancaria: Verifica si la carátula bancaria incluye la firma y el sello oficial del banco emisor para validar la autenticidad del documento. La misma figura como CADENA ORIGINAL DEL COMPLEMENTO DE CERTIFICACIÓN DIGITAL DEL SAT, SELLO DIGITAL DEL EMISOR y SELLO DIGITAL SAT. Si no se encuentran la firma y el sello, menciona que no se incluyeron en el documento proporcionado.
                     Return a JSON list."""
                 ),
                 HumanMessage(
@@ -247,3 +247,54 @@ async def process_acta(uploaded_file: UploadFile = File(...)):
         if 'temp_file_path' in locals():
             os.unlink(temp_file_path)
         raise HTTPException(status_code=500, detail=str(e))
+    
+        # Ruta para cargar y procesar el documento de ID
+@app.post("/process-ID/")
+async def process_acta(uploaded_file: UploadFile = File(...)):
+    try:
+        # Crear un archivo temporal para guardar el contenido del archivo cargado
+        with tempfile.NamedTemporaryFile(delete=False, suffix=".pdf") as temp_file:
+            content = await uploaded_file.read()  # Leer el contenido del archivo cargado
+            temp_file.write(content)  # Escribir el contenido en el archivo temporal
+            temp_file_path = temp_file.name  # Guardar la ruta del archivo temporal
+        
+        # Cargar el archivo Word utilizando PDFPlumberLoader
+        loader = PDFPlumberLoader(temp_file_path)
+        document = loader.load()[0]  # Suponiendo que solo hay un documento en el archivo
+        content_text = document.page_content
+        
+        # Procesar el contenido del documento con el modelo de OpenAI
+        chat = ChatOpenAI(model="gpt-4-0125-preview", temperature=0, openai_api_key=openai_api_key).bind(
+            response_format={"type": "json_object"}
+        )
+        output = chat.invoke(
+            [
+                SystemMessage(
+                    content="""Extrae la informacion de este documento de identificacion, ten en cuenta que fue pasado por OCR por tanto trata de adaptarte al formato y tenlo presente a la hora de extraer:
+                    Nombre Completo: Localiza y captura el nombre completo de la persona tal como aparece en el documento de identificación, recuerda que la persona puede tener dos apellidos y dos nombres, en el caso de que no sea legible aclaralo,
+                    Número de Identificación: Busca y registra el número único de identificación como el número CURP en (INE) o el número de pasaporte, en el caso de que no sea legible aclaralo,
+                    Fecha de expedición: La fecha en que se emitió el documento de identificación, en el caso de que no se encuentre aclaralo,
+                    Fecha de vigencia: La fecha de vencimiento del documento de identificación, en el caso de que no sea legible aclaralo.
+                    Return a JSON list."""
+                ),
+                HumanMessage(
+                    content=content_text
+                ),
+            ]
+        )
+        
+        # Convertir la respuesta en un diccionario de Python
+        info = json.loads(output.content)
+        
+        # Eliminar el archivo temporal
+        os.unlink(temp_file_path)
+       
+        # Devolver la respuesta
+        return {"info": info}
+    except Exception as e:
+        # Si algo sale mal, asegúrate de eliminar el archivo temporal
+        if 'temp_file_path' in locals():
+            os.unlink(temp_file_path)
+        raise HTTPException(status_code=500, detail=str(e))
+    
+    
